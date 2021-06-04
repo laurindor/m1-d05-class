4dd3778869da62b73258b629420bbefbae82be27
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-04 10:03:08 +0200

    Started code for JS OOP code along

--- 01_code-js-oop.js
+++ 01_code-js-oop.js
@@ -1,3 +1,22 @@
 /**
  * Code along for the lesson on JS Objects methods
  */
+
+const order1 = {
+  customer: "Ironhack",
+  beverage: "cappucino",
+  price: 10,
+  sugar: false,
+  extraFoam: true,
+};
+
+/*
+// From the losson about objects -> The reason for objects
+const customer = "Ironhack";
+let beverage = "cappucino";
+const price = 10;
+const sugar = false;
+const extraFoam = true;
+
+beverage = "caffé"; // this is not possible because it will overwrite the first one
+*/
